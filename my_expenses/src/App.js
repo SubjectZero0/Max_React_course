a90a8a6767003ab0fcd3d6d0c97e13d17fc4018c
Alexandrer Iordanou <iordanoualexander@gmail.com>
2022-12-02 22:12:06 +0200

    Implements ExpenseDate in App

--- my_expenses/src/App.js
+++ my_expenses/src/App.js
@@ -1,6 +1,5 @@
 //imports
-import ExpenseItem from "./Components/ExpenseItem";
-
+import ExpenseItem from "./Components/ExpenseComponents/ExpenseItem";
 //------------------------
 
 export class ExpenseItems {
@@ -9,21 +8,31 @@ export class ExpenseItems {
 	price;
 	item;
 	description;
+	date;
 
 	constructor(price, item, description) {
 		this.price = price;
 		this.item = item;
 		this.description = description;
+		this.date = new Date();
 	}
 }
 //create test Expense
 const testExpense = new ExpenseItems(10, "Water Bill", "Bill for having water");
+console.log(testExpense.date);
 
 function App() {
 	//render the page
 	return (
 		<div className="App">
-			<ExpenseItem price={testExpense.price} description={testExpense.description} item={testExpense.item}></ExpenseItem>
+			<div /*classname = "Card"*/>
+				<ExpenseItem
+					price={testExpense.price}
+					description={testExpense.description}
+					item={testExpense.item}
+					date={testExpense.date}
+				></ExpenseItem>
+			</div>
 		</div>
 	);
 }
