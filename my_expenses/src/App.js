550605cca62731b0bf820e83382bc87a05d5153e
Alexandrer Iordanou <iordanoualexander@gmail.com>
2022-12-03 12:50:50 +0200

    Implements Card to ExpenseItem and App

--- my_expenses/src/App.js
+++ my_expenses/src/App.js
@@ -19,20 +19,17 @@ export class ExpenseItems {
 }
 //create test Expense
 const testExpense = new ExpenseItems(10, "Water Bill", "Bill for having water");
-console.log(testExpense.date);
 
 function App() {
 	//render the page
 	return (
 		<div className="App">
-			<div /*classname = "Card"*/>
-				<ExpenseItem
-					price={testExpense.price}
-					description={testExpense.description}
-					item={testExpense.item}
-					date={testExpense.date}
-				></ExpenseItem>
-			</div>
+			<ExpenseItem
+				price={testExpense.price}
+				description={testExpense.description}
+				item={testExpense.item}
+				date={testExpense.date}
+			></ExpenseItem>
 		</div>
 	);
 }
--- my_expenses/src/Components/ExpenseComponents/ExpenseItem.js
+++ my_expenses/src/Components/ExpenseComponents/ExpenseItem.js
@@ -6,15 +6,16 @@ Expense Item Component
 //imports
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
 	return (
-		<div className="expense-item">
+		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<h2 className="expense-item__price">{props.price}</h2>
-			<h2 className="expense-item__description">{props.description}</h2>
 			<h2 className="expense-item">{props.item}</h2>
-		</div>
+			<h2 className="expense-item__description">{props.description}</h2>
+		</Card>
 	);
 }
 export default ExpenseItem;
