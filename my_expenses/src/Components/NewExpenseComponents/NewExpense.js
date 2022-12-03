c711be2fa78e40b62bf8522a400d6b8ebd75ad36
Alexandrer Iordanou <iordanoualexander@gmail.com>
2022-12-03 23:27:18 +0200

    Creates a New Expense component

---
+++ my_expenses/src/Components/NewExpenseComponents/NewExpense.js
@@ -0,0 +1,12 @@
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+
+function NewExpense(props) {
+	return (
+		<div className="new-expense">
+			<ExpenseForm />
+		</div>
+	);
+}
+
+export default NewExpense;
