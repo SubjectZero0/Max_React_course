3dfe37c452029dc74e0a0cdd51d05edc594f8d17
Alexandrer Iordanou <iordanoualexander@gmail.com>
2022-12-04 23:50:52 +0200

    refactors ExpenseItem to have a more concise look

--- my_expenses/src/Components/ExpenseComponents/ExpenseItem.js
+++ my_expenses/src/Components/ExpenseComponents/ExpenseItem.js
@@ -1,21 +1,19 @@
-//------------------------
-
 /*
 Expense Item Component
 */
 //imports
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
+//------------------------
 
 function ExpenseItem(props) {
 	return (
-		<Card className="expense-item">
+		<div className="expense-item">
 			<ExpenseDate date={props.date} />
-			<h2 className="expense-item__price">{props.price}</h2>
-			<h2 className="expense-item">{props.item}</h2>
+			<h2 className="expense-item__item">{props.item}</h2>
 			<h2 className="expense-item__description">{props.description}</h2>
-		</Card>
+			<div className="expense-item__price">{props.price}</div>
+		</div>
 	);
 }
 export default ExpenseItem;
