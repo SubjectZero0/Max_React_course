da838341a0a5483627f2e7e64386de1d1aed9e0d
Alexandrer Iordanou <iordanoualexander@gmail.com>
2022-12-04 23:52:33 +0200

    adds on submit event handler

--- my_expenses/src/Components/NewExpenseComponents/NewExpense.js
+++ my_expenses/src/Components/NewExpenseComponents/NewExpense.js
@@ -2,9 +2,20 @@ import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 function NewExpense(props) {
+	//get the data from the expense form and give them an id
+	const submitDataHandler = (enteredData) => {
+		const savedData = {
+			...enteredData,
+		};
+
+		//communicate with App.js.Then, send the final data to be rendered in Expenses.js.
+		props.onAddExpenseHandler(savedData);
+		return savedData;
+	};
+
 	return (
 		<div className="new-expense">
-			<ExpenseForm />
+			<ExpenseForm saveOnSubmitHandler={submitDataHandler} />
 		</div>
 	);
 }
