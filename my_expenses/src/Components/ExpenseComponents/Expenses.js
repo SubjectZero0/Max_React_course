0512e2e64015a2ccb1ebb1d657cbbe1e5fb74b1a
Alexandrer Iordanou <iordanoualexander@gmail.com>
2022-12-06 21:46:04 +0200

    Creates ExpensesFilter component that passes state to Expenses.js

--- my_expenses/src/Components/ExpenseComponents/Expenses.js
+++ my_expenses/src/Components/ExpenseComponents/Expenses.js
@@ -1,14 +1,24 @@
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "../ExpenseFilterComponents/ExpensesFilter";
+import { useState } from "react";
 
 function Expenses(props) {
-	console.log(props);
+	//state Hook. Initializes on 2022
+	const [year, setYear] = useState("2022");
+
+	//Gets the chosen year from child-ExpensesFilter.js
+	const changeYear = (chosenYearFilter) => {
+		setYear(chosenYearFilter);
+	};
+
 	return (
 		<div>
 			<Card className="expenses">
+				<ExpensesFilter onChangeYear={changeYear} />
 				{props.items.map((expense) => (
-					<ExpenseItem key={expense.item} item={expense.item} description={expense.description} price={expense.price} date={expense.date} />
+					<ExpenseItem key={expense.id} item={expense.item} description={expense.description} price={expense.price} date={expense.date} />
 				))}
 			</Card>
 		</div>
