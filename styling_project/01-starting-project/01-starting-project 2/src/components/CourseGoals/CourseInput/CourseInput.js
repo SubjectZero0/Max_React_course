a67008c9b4dc9fc1e23949b6a11e8a622a37e844
Alexandrer Iordanou <iordanoualexander@gmail.com>
2022-12-14 21:13:49 +0200

    Sets inline styles for the form, upon validation

--- styling_project/01-starting-project/01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.js
+++ styling_project/01-starting-project/01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,29 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+	const [enteredValue, setEnteredValue] = useState("");
+	const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+	const goalInputChangeHandler = (event) => {
+		if (event.target.value.trim().length > 0) {
+			setIsValid(true);
+		}
+		setEnteredValue(event.target.value);
+	};
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+	const formSubmitHandler = (event) => {
+		event.preventDefault();
+		if (enteredValue.trim().length === 0) {
+			setIsValid(false);
+			return;
+		}
+		props.onAddGoal(enteredValue);
+	};
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+	return (
+		<form onSubmit={formSubmitHandler}>
+			<div className="form-control">
+				<label style={{ color: isValid ? "black" : "red" }}>Course Goal</label>
+				<input type="text" onChange={goalInputChangeHandler} style={{ border: isValid ? "2px solid black" : "2px solid red" }} />
+			</div>
+			<Button type="submit">Add Goal</Button>
+		</form>
+	);
 };
 
 export default CourseInput;
