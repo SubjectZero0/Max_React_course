355a3501fe3906721638d99c1d19bf5679920b94
Alexandrer Iordanou <iordanoualexander@gmail.com>
2022-12-17 15:24:45 +0200

    Adds fragments and creates portal for ErrorModal

--- refs_project/src/App.js
+++ refs_project/src/App.js
@@ -1,26 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from "react";
 
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
-  const [usersList, setUsersList] = useState([]);
+	const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge) => {
-    setUsersList((prevUsersList) => {
-      return [
-        ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
-      ];
-    });
-  };
+	const addUserHandler = (uName, uAge) => {
+		setUsersList((prevUsersList) => {
+			return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toString() }];
+		});
+	};
 
-  return (
-    <div>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
-    </div>
-  );
+	return (
+		<Fragment>
+			<AddUser onAddUser={addUserHandler} />
+			<UsersList users={usersList} />
+		</Fragment>
+	);
 }
 
 export default App;
