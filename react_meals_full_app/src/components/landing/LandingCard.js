b1d07c46670737160b1e337e64c60636214c59e5
Alexandrer Iordanou <iordanoualexander@gmail.com>
2022-12-28 10:57:32 +0200

    Creates an introductory Card  component with custom css

---
+++ react_meals_full_app/src/components/landing/LandingCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./LandingCard.module.css";
+
+const LandingCard = () => {
+	return (
+		<div className={styles.card__container}>
+			<h1 className={styles.card__text}>WERWEORIKWPEORIPWEORIWPOERI</h1>
+		</div>
+	);
+};
+
+export default LandingCard;
