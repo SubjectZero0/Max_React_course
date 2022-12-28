68508025be7b75f4843796aea493897fd740d373
Alexandrer Iordanou <iordanoualexander@gmail.com>
2022-12-28 10:57:11 +0200

    Creates Main header  component with custom css

---
+++ react_meals_full_app/src/components/header/MainHeader.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import NavBar from "./NavBar";
+import styles from "./MainHeader.module.css";
+
+const MainHeader = () => {
+	return (
+		<Fragment>
+			<NavBar />
+			<div className={styles.main_header__image}>
+				<img src="/platter.jpg" />
+			</div>
+		</Fragment>
+	);
+};
+
+export default MainHeader;
