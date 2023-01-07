a232163d183e9131e51e39cc0b606934768443a6
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-01-07 19:16:07 +0200

    Adds a card as an introduction

--- react_meals_full_app/src/components/landing/LandingCard.js
+++ react_meals_full_app/src/components/landing/LandingCard.js
@@ -4,7 +4,13 @@ import styles from "./LandingCard.module.css";
 const LandingCard = () => {
 	return (
 		<div className={styles.card__container}>
-			<h1 className={styles.card__text}>WERWEORIKWPEORIPWEORIWPOERI</h1>
+			<h1 className={styles.card__text}>Delicious food, Delivered to your door!</h1>
+			<p className={styles.card__text}>
+				Choose your favourite meal from our broad selection of available meals and enjoy a delicious lunch, or dinner, at home.
+			</p>
+			<p className={styles.card__text}>
+				All our meals are cooked with high quality ingredients, just in time and, of course, by our experienced chefs!
+			</p>
 		</div>
 	);
 };
