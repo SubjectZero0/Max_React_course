d34b63955d2e04bd80a49c028bbd0a9144b8f3d8
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-01-08 16:16:11 +0200

    Creates MenuItem component jsx

--- react_meals_full_app/src/components/menu/MenuItem.js
+++ react_meals_full_app/src/components/menu/MenuItem.js
@@ -1,7 +1,25 @@
-import React from "react";
+import { React, Fragment } from "react";
+import styles from "./MenuItem.module.css";
+import AddButton from "./AddButton";
 
-const MenuItem = () => {
-	return <div>MenuItem</div>;
+// ------------------------------------------------------------
+
+const MenuItem = (props) => {
+	return (
+		<Fragment>
+			<div className={styles.menu_items__container}>
+				<div className={styles.menu_items__item}>
+					<h3>{props.item.foodName}</h3>
+					<p>{props.item.foodDesc}</p>
+					<h3 className={styles.menu_items__item_price}>{props.item.foodPrice}</h3>
+				</div>
+
+				<div className={styles.menu_items__form}>
+					<AddButton />
+				</div>
+			</div>
+		</Fragment>
+	);
 };
 
 export default MenuItem;
