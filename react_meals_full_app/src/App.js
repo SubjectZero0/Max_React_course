221182b5137ba9e9ae3e5b5e96a3aed325b92c54
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-01-08 11:53:16 +0200

    Includes MenuContainer in jsx

--- react_meals_full_app/src/App.js
+++ react_meals_full_app/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import MainHeader from "./components/header/MainHeader";
 import LandingCard from "./components/landing/LandingCard";
+import MenuContainer from "./components/menu/MenuContainer";
+
+// -------------------------------------------
 
 function App() {
 	return (
 		<React.Fragment>
 			<MainHeader />
 			<LandingCard />
+			<MenuContainer />
 		</React.Fragment>
 	);
 }
